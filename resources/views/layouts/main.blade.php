afc9c11fb6d739e3a539b6b3430140a119945e9b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-12 16:20:16 +0100

    added partial header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,15 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{Vite::asset('resources/img/treni.webp')}}">
     <title>{{env('APP_NAME', 'Trains')}} | @yield('title')</title>
+    {{--Assets Frontend--}}
     @vite('resources/js/app.js')
     {{--Cdns--}}
     @yield('cdns')
 </head>
 <body>
 
+    {{--Header--}}
+    @include('includes.header')
+
      {{--Main Content--}}
      @yield('main-content')
-    
+     
+    {{--Footer--}}
+    <footer></footer> 
 </body>
  {{-- Scripts --}}
  @yield('cdns')
